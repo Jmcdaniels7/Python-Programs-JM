c8c8aced66a39bdecf4ea2658b93a250d445a0f1
Jacob McDaniels <jamcdaniels99@gmail.com>
2024-09-03 18:29:02 -0400

    new

---
+++ Random python files/cars.py
@@ -0,0 +1,31 @@
+class Vehicle:
+    def __init__(self, carType):
+        self.car = carType
+
+    def getCarType(self):
+        return self.car
+
+class Automobile(Vehicle):
+    def __init__(self, carType, year, make, model, doors, roof):
+        Vehicle.__init__(self, carType)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+    def vehicleInput(self):
+        print(f"Vehicle Type: {self.car}\nYear: {self.year}\nMake: {self.make}\nModel: {self.model}\nNumber of Doors: {self.doors}\nType of Roof: {self.roof}")
+
+
+
+carType = input("What is the type of the vehicle?")
+year = input("What year is the vehicle?")
+make = input("What is the make of the vehicle?")
+model = input("What is the model of the vehicle?")
+doors = input("Is the vehicle a 2 or 4 door ?")
+roof = input("Does the car have a sunroof?")
+
+v = Automobile(carType, year, make, model, doors, roof)
+
+v.vehicleInput()
---
+++ Random python files/sort.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sort012(self, numbers):
+        numbers = [0,2,1,2,0]
+
+        numbers.sort()
+
+        print(numbers)
+
+numbers = [0,2,1,2,0]
+p = Solution()
+
+p.sort012(numbers)
